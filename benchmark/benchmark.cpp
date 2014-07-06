171d8952f98cdd4c5b6e79c01cf46dcfa6589817
FrankHB <frankhb1989@gmail.com>
2014-07-06 22:17:39 +0800

    Enabled extended benchmarks for EvilTwin Observer.
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -3,6 +3,10 @@
 #include "bs1_benchmark.hpp"
 #include "bs2_benchmark.hpp"
 #include "evl_benchmark.hpp"
+#if USE_EvilTwin_Ext
+#	include "evf_benchmark.hpp"
+#	include "evs_benchmark.hpp"
+#endif
 #include "jls_benchmark.hpp"
 #include "nss_benchmark.hpp"
 #include "s11_benchmark.hpp"
@@ -85,6 +89,10 @@ int main(int argc, char* argv[])
 		test<Bs1>("Boost Signals", records, i);
 		test<Bs2>("Boost Signals2", records, i);
 		test<Evl>("EvilTwin Observer", records, i);
+#if USE_EvilTwin_Ext
+		test<Evf>("EvilTwin Observer Fork", records, i);
+		test<Evs>("EvilTwin Observer Safe", records, i);
+#endif
 		test<Jls>("Jl_signal", records, i);
 		test<Nss>("Nano-signal-slot", records, i);
 		test<S11>("amc522 Signal11", records, i);
