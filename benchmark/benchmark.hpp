108a0b56de935531f64d3d4946893c6f4aef66de
FrankHB <frankhb1989@gmail.com>
2014-07-06 23:24:19 +0800

    Added templated benchmark.
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- benchmark/benchmark.hpp
+++ benchmark/benchmark.hpp
@@ -72,4 +72,140 @@ make_random_sequence(size_t n)
 	return std::move(vec);
 }
 
+
+namespace Benchmark
+{
+
+struct NoTrack
+{};
+
+
+template<class Subject, class Base = NoTrack>
+class SignalSlotBenchmark : public Base
+{
+public:
+	using Foo = Benchmark::SignalSlotBenchmark<Subject, Base>;
+    static chrono_timer timer;
+
+	NOINLINE(void handler(Rng_t& rng))
+    {
+        volatile std::size_t a = rng();
+
+		static_cast<void>(a);
+    }
+
+	NOINLINE(static void
+		do_connect(Subject& subject, Foo& foo))
+	{
+		connect(subject, std::bind(&Foo::handler, &foo, std::placeholders::_1));
+	}
+
+    NOINLINE(static double construction(std::size_t N))
+    {
+        std::size_t count = 1, elapsed = 0;
+
+        for (; elapsed < g_limit; ++count)
+        {
+            timer.reset();
+
+			std::unique_ptr<Subject> subject(new Subject());
+            std::vector<Foo> foo_array(N);
+
+			do_connect(*subject, std::ref(foo_array.back()));
+            elapsed += timer.count<Timer_u>();
+        }
+        return N / std::chrono::duration_cast<Delta_u>
+            (Timer_u(g_limit / count)).count();
+    }
+
+	NOINLINE(static double destruction(std::size_t N))
+    {
+        auto randomized(make_random_sequence<std::size_t, Rng_t>(N));
+		Rng_t rng;
+        std::size_t count = 1, elapsed = 0;
+
+        for (; elapsed < g_limit; ++count)
+        {
+            {
+                std::unique_ptr<Subject> subject(new Subject());
+                std::vector<Foo> foo_array(N);
+
+                for (auto index : randomized)
+					Foo::do_connect(*subject, std::ref(foo_array[index]));
+                timer.reset();
+            }
+            elapsed += timer.count<Timer_u>();
+        }
+        return N / std::chrono::duration_cast<Delta_u>
+            (Timer_u(g_limit / count)).count();
+    }
+
+    NOINLINE(static double connection(std::size_t N))
+    {
+        auto randomized(make_random_sequence<std::size_t, Rng_t>(N));
+		Rng_t rng;
+        std::size_t count = 1, elapsed = 0;
+
+        for (; elapsed < g_limit; ++count)
+        {
+            Subject subject;
+            std::vector<Foo> foo_array(N);
+
+            timer.reset();
+            for (auto index : randomized)
+				do_connect(subject, std::ref(foo_array[index]));
+            elapsed += timer.count<Timer_u>();
+        }
+        return N / std::chrono::duration_cast<Delta_u>
+            (Timer_u(g_limit / count)).count();
+    }
+
+    NOINLINE(static double emission(std::size_t N))
+    {
+        auto randomized(make_random_sequence<std::size_t, Rng_t>(N));
+		Rng_t rng;
+        std::size_t count = 1, elapsed = 0;
+
+        for (; elapsed < g_limit; ++count)
+        {
+            Subject subject;
+            std::vector<Foo> foo_array(N);
+
+            for (auto index : randomized)
+				do_connect(subject, std::ref(foo_array[index]));
+            timer.reset();
+            subject(rng);
+            elapsed += timer.count<Timer_u>();
+        }
+        return N / std::chrono::duration_cast<Delta_u>
+            (Timer_u(g_limit / count)).count();
+    }
+
+    NOINLINE(static double combined(std::size_t N))
+    {
+        auto randomized(make_random_sequence<std::size_t, Rng_t>(N));
+		Rng_t rng;
+        std::size_t count = 1;
+
+        timer.reset();
+
+        for (; g_limit > timer.count<Timer_u>(); ++count)
+        {
+            Subject subject;
+            std::vector<Foo> foo_array(N);
+
+            for (auto index : randomized)
+				do_connect(subject, std::ref(foo_array[index]));
+            subject(rng);
+        }
+        return N / std::chrono::duration_cast<Delta_u>
+            (Timer_u(g_limit / count)).count();
+    }
+};
+
+template<class Subject, class Base>
+chrono_timer SignalSlotBenchmark<Subject, Base>::timer;
+
+} // namespace Benchmark;
+
 #endif // BENCHMARK_HPP
